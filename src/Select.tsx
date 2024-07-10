53318eb037d6c9e993faa07fd22d288811570997
22fi063 <22fi063@gmail.com>
2024-07-10 22:59:40 +0900

    a

--- src/Select.tsx
+++ src/Select.tsx
@@ -4,7 +4,7 @@ function Select() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl mb-4">
-       当てはまる方を選んでください
+        当てはまる方を選んでください
       </h1>
       <RouterLink to="/join" className="no-underline">
         <button className="bg-blue-500 text-white py-2 px-4 rounded-lg text-lg hover:bg-blue-700">
